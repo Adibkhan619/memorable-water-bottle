43471531f4ca3b18b18c283fa1b57375743e6f09
Adibkhan619 <work.adibkhan@gmail.com>
2024-03-12 22:45:39 +0600

    added cart option and shown into local storage

--- src/Components/Bottle/Bottle.jsx
+++ src/Components/Bottle/Bottle.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './bottle.css'
-const Bottle = ({bottle}) => {
+
+
+const Bottle = ({bottle , handleAddToCart}) => {
     const {name, price, img} = bottle;
 
     return (
         <div className='bottle'>
-            <h2>Name: {name}</h2>
+            <h2>{name}</h2>
             <img src={img} alt="" />
             <h3>Price: ${price}</h3>
+            <button onClick={() => handleAddToCart(bottle)}>Purchase</button>
         </div>
     );
 };
--- src/Components/Bottles/Bottles.jsx
+++ src/Components/Bottles/Bottles.jsx
@@ -3,27 +3,43 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 import Bottle from '../Bottle/Bottle';
 import './bottles.css'
+import { addToLS } from '../../utilities/localStorage';
 
 
 const Bottles = () => {
 
-    const [bottles, setBottles] = useState([])
+    const [bottles, setBottles] = useState([]);
+    const [cart, setCart] = useState([]);
 
     useEffect(() => {
         fetch('bottle.json')
             .then(res => res.json())
             .then(data => setBottles(data));
-            
     }, [])
+
+    const handleAddToCart = bottle => {
+        const newCart = [...cart, bottle];
+        setCart(newCart);
+        addToLS(bottle.id);
+    }
 
     return (
         <div >
+            <h2>Bottle Available: {bottles.length}</h2>
+            <h3>Cart: {cart.length}</h3>
+            <ul>
+                {cart.map(cart => <li key={cart.id}>{cart.name}</li>)}
+            </ul>
             <div className='bottle-container'>
-                {
-                bottles.map(bottle => <Bottle bottle={bottle} key={bottle.id}></Bottle>)
-            }
+                {              
+               bottles.map(bottle => <Bottle
+                        bottle={bottle}
+                        key={bottle.id}
+                        handleAddToCart={handleAddToCart}
+                    ></Bottle>)
+                }
             </div>
-            
+
         </div>
     );
 };
